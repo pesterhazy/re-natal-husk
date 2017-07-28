acdfc8834ef2af98487f25f37f91463e437ea11d
Paulus Esterhazy <pesterhazy@gmail.com>
2017-07-28 21:37:25 +0200

    WIP

---
+++ index.ios.shell.js
@@ -0,0 +1,11 @@
+(function (require){
+    RE_NATAL_PLACEHOLDER()
+})(function (name) {
+    var modules={'react-native': require('react-native'), 'react': require('react')};modules['./images/cljs.png']=require('./images/cljs.png');
+    if (modules[name]){
+        return modules[name];
+    }
+    else {
+        console.error("Not found:", name);
+    }
+});

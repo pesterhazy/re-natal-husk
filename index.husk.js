123a3cf59e9d4d53a5f5633afd9b47da81edf218
Paulus Esterhazy <pesterhazy@gmail.com>
2017-07-31 22:49:08 +0200

    Simplify

--- index.husk.js
+++ index.husk.js
@@ -1,7 +1,5 @@
-(function (require){
-    RE_NATAL_PLACEHOLDER(require);
-})(function (name) {
-    var modules={'react-native': require('react-native'), 'react': require('react')};modules['./images/cljs.png']=require('./images/cljs.png');
+RE_NATAL_PLACEHOLDER(function (name) {
+    var modules=require('./husk-requires');modules['./images/cljs.png']=require('./images/cljs.png');
     if (modules[name]){
         return modules[name];
     }
